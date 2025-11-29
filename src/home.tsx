4269ab067ed40313ebcb8808e0e0be6b572d5be1
Lanzhijiang <lanzhijiang@foxmail.com>
2025-11-29 15:59:49 +0800

    fix(home): ArticleList content overflowed on mobile

--- src/home.tsx
+++ src/home.tsx
@@ -5,7 +5,7 @@ export function Home() {
   const articleList = getAllArticles();
 
   return (
-    <div className="bg-sys-bg-primary relative min-h-screen box-border px-[1.5rem] py-[2rem] md:pl-[3rem] md:pr-[2rem] md:py-[3rem] flex flex-col md:flex-row gap-[2rem] md:gap-[4rem]">
+    <div className="bg-sys-bg-primary relative h-screen box-border px-[1.5rem] py-[2rem] md:pl-[3rem] md:pr-[2rem] md:py-[3rem] flex flex-col md:flex-row gap-[2rem] md:gap-[4rem]">
       <div className="flex flex-shrink-0 flex-col justify-between">
         {/* Intro */}
         <div className="flex flex-col">
@@ -15,9 +15,6 @@ export function Home() {
           {/* Handle */}
           <div className="relative font-bold font-size-[3.5rem] md:font-size-[5.5rem]">
             <span className="color-sys-text-brand">蓝汁酱</span>
-            {/* <span className="w-max text-transparent text-stroke-[1px] text-stroke-[#505050] absolute left-[0.6rem] top-[0.6rem] hover:left-0 hover:top-0 transition-all duration-300">
-              蓝汁酱
-            </span> */}
           </div>
         </div>
 
@@ -30,7 +27,7 @@ export function Home() {
       </div>
 
       {/* Content Area */}
-      <div className="p-3 box-border flex flex-col items-center gap-[1.5rem] flex-1 max-h-[90vh] overflow-auto">
+      <div className="p-3 box-border flex flex-col items-center gap-[1.5rem] flex-1 min-h-0 overflow-hidden">
         <ArticleList articles={articleList} />
       </div>
     </div>
